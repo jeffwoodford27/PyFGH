22b5ad6702279fb75122162eaf04a68e7bf608bf
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2021-06-03 21:36:18 -0500

    Updated the clear button to work properly. Add another feature to the Calculate button.

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 import multiprocessing
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 """
 The code in this file is for a gui (graphic user interface) application. This code is written with the tkinter library framework.
@@ -29,6 +29,7 @@ ttk.Label(window, text="Computer Cores:",
 n = tk.StringVar()
 cores = ttk.Combobox(window, width=10, textvariable=n)
 
+
 # definition for calculating core counts. Needs to include numbers leading up to the core count. Need to implement a
 # better for loop.
 def cpu_count():
@@ -42,7 +43,7 @@ m = tk.StringVar()
 molecule = ttk.Combobox(window, width=10, textvariable=m)
 
 # creates values inside of the choice box
-molecule["values"] = (u'H\u2082O')
+molecule["values"] = u'H\u2082O'
 
 # Label
 ttk.Label(window, text="Q Choice Box:", font=("Times New Roman", 10)).place(x=475, y=50)
@@ -51,7 +52,8 @@ q_equations = ttk.Combobox(window, width=22, textvariable=n)
 
 # creates values inside of the choice box
 q_equations["values"] = (u'q\u2081=r\u2081-r\u2081\u2070=>q\u2082=r\u2082-r\u2082\u2070', u'q=\u00BD(q\u2081+q\u2082)=>'
-                        u'q=\u00BD(q\u2081-q\u2082)', u'q=\u03B8-\u03B8\u2070=>q=cos(\u03B8-\u03B8\u2070)')
+                                                                                          u'q=\u00BD(q\u2081-q\u2082)',
+                         u'q=\u03B8-\u03B8\u2070=>q=cos(\u03B8-\u03B8\u2070)')
 
 # Label
 ttk.Label(window, text="Equilibrium Coordinates:", font=("Times New Roman", 10)).place(x=745, y=50)
@@ -75,28 +77,45 @@ c = tk.StringVar()
 g = ttk.Combobox(window, width=15, textvariable=c)
 
 # creates values inside of the choice box
-g["values"] = ('test')
+g["values"] = 'test'
 
 # Label
 ttk.Label(window, text="V:", font=("Times New Roman", 20)).place(x=675, y=100)
 d = tk.StringVar()
 v = ttk.Combobox(window, width=32, textvariable=d)
 
-v["values"] = 'Model-Harmonic Oscillator', 'Model-Morse Oscillator', 'Read Structures and Energies from File', 'Compute on The Fly'
+v[
+    "values"] = 'Model-Harmonic Oscillator', 'Model-Morse Oscillator', 'Read Structures and Energies from File', 'Compute on The Fly'
 
 # Button
-exit = tk.Button(window, text ='Exit', bd ='10', bg='red', fg='white',
-                command = window.destroy).place(x=385, y= 180)
+exit = tk.Button(window, text='Exit', bd='10', bg='red', fg='white',
+                 command=window.destroy).place(x=385, y=180)
 
 
-calculate = tk.Button(window, text ='Calculate', bd ='20', bg='green', fg='white',
-                command = window.destroy).place(x=450, y= 170)
+def output():
+    messagebox.askyesno("PyFGH", "Would you like to save the data to a text file?")
 
-clear = tk.Button(window, text ='Clear', bd ='10', bg='blue', fg='white',
-                command = window.destroy).place(x=565, y= 180)
+
+calculate = tk.Button(window, text='Calculate', bd='20', bg='green', fg='white',
+                      command=output).place(x=450, y=170)
+
+
+# This will clear the data in the choice boxes
+def clear_data():
+    cores.set('')
+    molecule.set('')
+    q_equations.set('')
+    equilibrium.set('')
+    t.set('')
+    g.set('')
+    v.set('')
+
+
+clear = tk.Button(window, text='Clear', bd='10', bg='blue', fg='white',
+                  command=clear_data).place(x=565, y=180)
 
 # Adding combobox drop down list
-# monthchoosen['values'] = (multiprocessing.cpu_count())
+# cores['values'] = (multiprocessing.cpu_count())
 cpu_count()
 cores.place(x=105, y=50)
 cores.current()

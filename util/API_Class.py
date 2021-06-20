330c58774b60517a728896e9b5f108422c9fdb95
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2021-06-20 00:32:39 -0500

    Updated my code to save variables to objects that can be accessed throughout files!

---
+++ util/API_Class.py
@@ -0,0 +1,21 @@
+class outputAPI:
+
+    class items:
+
+        molecule = 0
+        q_equation1 = 0
+        q_equation2 = 0
+        q_equation3 = 0
+        text1 = 0
+        text2 = 0
+        text3 = 0
+        text4 = 0
+        text5 = 0
+        text6 = 0
+        t = 0
+        g = 0
+        v1 = 0
+        v2 = 0
+        v3 = 0
+        file_name = 0
+

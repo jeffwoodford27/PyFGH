350784cb8356c7a8e8c73aab1f55a5e4e1dc5054
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2021-07-15 22:02:36 -0500

    Updated my code.

--- test1.py
+++ test1.py
@@ -10,11 +10,23 @@ from multiprocessing import Process, Lock, Queue
 # This is the parent process
 def parent(q):
     GUI.main_window()
-    print('The interface is started')
-    print(os.getpid())
+    print('The interface is started Process: ', os.getpid())
     holder = DataObject.InputData()
+    holder.setMolecule(DataObject.holdData.molecule)
+    holder.setQ1(DataObject.holdData.q_equation1)
+    holder.setQ2(DataObject.holdData.q_equation2)
+    holder.setQ3(DataObject.holdData.q_equation3)
     holder.setN1(DataObject.holdData.N1)
+    holder.setL1(DataObject.holdData.L1)
     holder.setN2(DataObject.holdData.N2)
+    holder.setL2(DataObject.holdData.L2)
+    holder.setN3(DataObject.holdData.N3)
+    holder.setL3(DataObject.holdData.L3)
+    holder.setT(DataObject.holdData.t)
+    holder.setG(DataObject.holdData.g)
+    holder.setV(DataObject.holdData.v)
+    holder.setFileName(DataObject.holdData.file_name)
+    # holder.setModelData(DataObject.holdData.model_data)
     q.put(holder)
     return
 
@@ -23,9 +35,24 @@ def parent(q):
 # Do something then pass the results back to the parent
 # child back to the parent
 def child1(q):
-    print('This is the child process ', os.getpid())
+    print('This is the child process: ', os.getpid())
     holder1 = q.get()
-    print(holder1.N1, holder1.N2)
+    print("Molecule: ", holder1.molecule, " This is from the child process")
+    print("Q1 equation: ", holder1.q_equation1, " This is from the child process")
+    print("Q2 equation: ", holder1.q_equation2, " This is from the child process")
+    print("Q3 equation: ", holder1.q_equation3, " This is from the child process")
+    print("N1: ", holder1.N1, " This is from the child process")
+    print("L1: ", holder1.L1, " This is from the child process")
+    print("N2: ", holder1.N2, " This is from the child process")
+    print("L2: ", holder1.L2, " This is from the child process")
+    print("N3: ", holder1.N3, " This is from the child process")
+    print("L3: ", holder1.L3, " This is from the child process")
+    print("T : ", holder1.t, " This is from the child process")
+    print("G : ", holder1.g, " This is from the child process")
+    print("V : ", holder1.v, " This is from the child process")
+    # print("File Name : ", holder1.file_name, " This is from the child process")
+    # print("Model Data : ", holder1.model_data, " This is from the child process")
+
     return
 
 

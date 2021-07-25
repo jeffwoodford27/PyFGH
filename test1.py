2993c9911ad6141e9ae68626d92645aa82173ccb
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2021-07-25 17:45:15 -0500

    Updated my code. I added an SSH feature to my GUI interface. It runs the GUI locally and saves the input to a text file. I then sent  that text file to a remote server. I then add two numbers based on the input from N1 and L1 on the remote server. The remote server then sends the results back to the local host in a text file. I then put this data in the resources folder.

--- test1.py
+++ test1.py
@@ -1,12 +1,14 @@
 import os
 import time
-
+import paramiko
 import GUI
 from util import DataObject
 from multiprocessing import Process, Queue
 
 """
 This one uses Queues
+This communicates by using SSH
+Also connected to test3.py
 """
 
 
@@ -39,12 +41,57 @@ def datamuncher(q):
     print("Filename: ", holder1.file_name)
     print("V : ", holder1.v)
     print("Values from the sum of N and L: ", holder1.sum)
+    data = [holder1.molecule, holder1.q_equation1, holder1.q_equation2,
+            holder1.q_equation3, holder1.N1, holder1.L1, holder1.N2,
+            holder1.L2, holder1.N3, holder1.L3, holder1.t,
+            holder1.g, holder1.v]
+    save_path = "./resources/"
+    file_name = "DataList.txt"
+    completeName = os.path.join(save_path, file_name)
+    file = open(completeName, "w", encoding="utf-8")
+    for x in data:
+        file.write('%s\n' % x)
+    file.close()
+
+
+
+    host = holder1.host
+    port = 22
+    username = holder1.user
+    password = holder1.password
+
+
+    command = "python3 test3.py"
+    command2 = "rm DataList.txt"
+    command3 = "rm test3.py"
+    command4 = "rm Results.txt"
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port, username, password)
+    sftp = ssh.open_sftp()
+
+    path = "/home/jrandleman/DataList.txt"
+    localpath = "./resources/DataList.txt"
+    sftp.put(localpath, path)
+    path2 = "/home/jrandleman/test3.py"
+    localpath2 = "test3.py"
+    sftp.put(localpath2, path2)
+    stdin, stdout, stderr = ssh.exec_command(command)
+    stdin, stdout, stderr = ssh.exec_command(command2)
+    stdin, stdout, stderr = ssh.exec_command(command3)
+    path3 = "/home/jrandleman/Results.txt"
+    localpath3 = "./resources/Results.txt"
+    sftp.get(path3, localpath3)
+    stdin, stdout, stderr = ssh.exec_command(command4)
+    sftp.close()
+    ssh.close()
+
     Charles = Bob()
     Charles.setSum(holder1.sum)
     q.put(Charles)
     # print("File Name : ", holder1.file_name, " This is from the child process")
     # print("Model Data : ", holder1.model_data, " This is from the child process")
-
     return
 
 
@@ -75,10 +122,14 @@ def datagrabber():
         int(DataObject.holdData.L2) + int(DataObject.holdData.L3)
     holder.setMessage("This is from the child")
     holder.set_sum(v)
-    # holder.setModelData(DataObject.holdData.model_data)  # look into pickling possibly un-pickling
+    holder.set_host(DataObject.holdData.host)
+    holder.set_user(DataObject.holdData.user)
+    holder.set_password(DataObject.holdData.password)
+    #holder.setModelData(DataObject.holdData.model_data)  # look into pickling possibly un-pickling
     q.put(holder)
     Charles = q.get()
     print(Charles.sum)
+
     return
 
 

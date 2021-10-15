433802bf932d0c539d4778318f999d7b90b82c4e
unknown <nelsonmaxey@gmail.com>
2021-10-15 10:02:15 -0500

    VMatrix, TMatrix, and main to use DataHandler object

--- Vmatrix.py
+++ Vmatrix.py
@@ -2,6 +2,8 @@
 import numpy as np
 from util import pyfghutil
 import math
+import multiprocessing as mp
+import sys
 
 
 class HarmonicOscillatorModel:
@@ -21,11 +23,129 @@ class Molecule:
         self.Vtype = Vtype
         self.Vmodel = Vmodel
 
+class NBlock:
+    def __init__(self, D, NValues):
+        self.D = D
+        self.N = NValues[D-1]
+        self.SmallestN = NValues[0]
+        self.NProd = np.prod(NValues[:D-1])
+        self.diagblocks = []
+        self.difblocks = []
+        self.xboffset = 0
+        self.yboffset = 0
+        #print(NValues)
+        #print("D and NProd"+str(self.D)+" "+str(self.NProd))
+        #print("Smallest N:"+str(self.SmallestN))
+        #We assume that blocks is an (Nx+1)^2 grid for blocks below it
+        #Shape is (y, x) of the array
+        #NDarray uses [y,x] accessing
+        #NProd will be the amount of items in alpha/beta it encompases for a block
+        if(self.D > 1):
+            self.diagblocks = [None]*self.N
+            self.difblocks = [None]*(self.N-1)
+            for x in range(self.N):
+                self.diagblocks[x] = NBlock(D-1, NValues)
+                    
+            for y in range(self.N-1):
+                self.difblocks[y] = NBlock(D-1, NValues)
+                
+        else:
+            self.difblocks = np.zeros((self.N, self.N), float)
+    def difSetup(self):
+        if(self.D>1):
+            for x in range(self.N):
+                self.diagblocks[x].xboffset = self.xboffset + int(x * (self.NProd/self.SmallestN))
+                self.diagblocks[x].yboffset = self.yboffset + int(x * (self.NProd/self.SmallestN))
+                self.diagblocks[x].difSetup()
+                #print("Setting diag block: "+str(self.D-1)+" with xboffset "+str(self.xboffset + int(x * (self.NProd/self.SmallestN))))
+                #print("Setting diag block: "+str(self.D-1)+" with yboffset "+str(self.yboffset + int(x * (self.NProd/self.SmallestN))))
+                        
+            for y in range(self.N-1):
+                self.difblocks[y].xboffset = self.xboffset + 0
+                self.difblocks[y].yboffset = self.yboffset + 1+int(y * (self.NProd/self.SmallestN))
+                self.difblocks[y].difSetup()
+                #print("Setting diag block: "+str(self.D-1)+" with xboffset "+str(self.xboffset))
+                #print("Setting diag block: "+str(self.D-1)+" with yboffset "+str(self.yboffset + 1+int(y * (self.NProd/self.SmallestN))))
+                
+    def coordGen(self):
+        if(self.D>1):
+            coords = []
+            for x in range(len(self.diagblocks)):
+                coords += self.diagblocks[x].coordGen()
+            for y in range(len(self.difblocks)):
+                coords += self.difblocks[y].coordGen()
+            return(coords)
+        elif(self.D == 1):
+            return([(self.xboffset, self.yboffset)])
+            
+    def print1x1(self):
+        if(self.D != 1):
+            '''
+            print("Print dimension class "+str(self.D-1)+" with N "+str(self.blocks[0,0].N)+"and with nprod "+str(self.blocks[0,0].NProd))
+            print(self.blocks[0,0].blocks)
+            self.blocks[0,0].print1x1()
+            '''
+        else:
+            pass
+    def accessPoint(self, alpha, beta):
+        if(self.D > 1):
+            return(self.blocks[beta//self.NProd, alpha//self.NProd].accessPoint(beta%self.NProd, alpha%self.NProd))
+        else:
+            return(self.blocks[alpha, beta])
+    def setBlock(self, N1BlockX, N1BlockY, blockData, startFlag=True):
+        if(startFlag):
+            if(self.D > 1):
+                xb = (N1BlockX*self.SmallestN)//self.NProd
+                yb = (N1BlockY*self.SmallestN)//self.NProd
+                #print("Set block (D,xb,yb): "+str(self.D)+", "+str(xb)+", "+str(yb))
+                if(xb == yb):
+                    self.diagblocks[xb].setBlock((N1BlockX*self.SmallestN)%self.NProd, (N1BlockY*self.SmallestN)%self.NProd, blockData, False)
+                else:
+                    xydif = abs(xb-yb)
+                    self.difblocks[xydif-1].setBlock((N1BlockX*self.SmallestN)%self.NProd, (N1BlockY*self.SmallestN)%self.NProd, blockData, False)
+            else:
+                self.difblocks = blockData
+        else:
+            if(self.D > 1):
+                xb = N1BlockX//self.NProd
+                yb = N1BlockY//self.NProd
+                #print("Set block (D,xb,yb): "+str(self.D)+", "+str(xb)+", "+str(yb))
+                if(xb == yb):
+                    self.diagblocks[xb].setBlock(N1BlockX%self.NProd, N1BlockY%self.NProd, blockData, False)
+                else:
+                    xydif = abs(xb-yb)
+                    self.difblocks[xydif-1].setBlock(N1BlockX%self.NProd, N1BlockY%self.NProd, blockData, False)
+            else:
+                self.difblocks = blockData
+    def readBlock(self, N1BlockX, N1BlockY, startFlag=True):
+        if(startFlag):
+            if(self.D > 1):
+                xb = (N1BlockX*self.SmallestN)//self.NProd
+                yb = (N1BlockY*self.SmallestN)//self.NProd
+                if(xb == yb):
+                    return(self.diagblocks[xb].readBlock((N1BlockX*self.SmallestN)%self.NProd, (N1BlockY*self.SmallestN)%self.NProd, False))
+                else:
+                    xydif = abs(xb-yb)
+                    return(self.difblocks[xydif-1].readBlock((N1BlockX*self.SmallestN)%self.NProd, (N1BlockY*self.SmallestN)%self.NProd, False))
+            else:
+                return(self.difblocks)
+        else:
+            if(self.D > 1):
+                xb = N1BlockX//self.NProd
+                yb = N1BlockY//self.NProd
+                if(xb == yb):
+                    return(self.diagblocks[xb].readBlock(N1BlockX%self.NProd, N1BlockY%self.NProd, False))
+                else:
+                    xydif = abs(xb-yb)
+                    return(self.difblocks[xydif-1].readBlock(N1BlockX%self.NProd, N1BlockY%self.NProd, False))
+            else:
+                return(self.difblocks)
+
 
 
 
 #A function to calculate the invidivdual values for the VMatrix
-def Vab(d, NValue, LValue, VModel, VType, deltax, dimensionCounterArray):
+def Vab(d, NValue, LValue, VType, VModel, deltax, dimensionCounterArray):
     #Deltacounter is used to makes sure that the value being calculated is in the diagonal of the matrix
     Deltacounter = 0
     #Total is the value returned for the calculation
@@ -42,48 +162,144 @@ def Vab(d, NValue, LValue, VModel, VType, deltax, dimensionCounterArray):
             realdeltax = (float(LValue[(d-1)-counter])/float(NValue[(d-1)-counter]))
             if(VType[counter] == 0):
                 xj = ((dimensionCounterArray[(counter*2)+1])*realdeltax)+(realdeltax/2.0)
-                total += 0.5*VModel[(d-1)-counter].k*(xj-(LValue[(d-1)-counter]*0.5))**2
+                total += 0.5*VModel[(d-1)-counter][1]*(xj-(LValue[(d-1)-counter]*0.5))**2
             elif(VType[counter] == 1):
                 xj = ((dimensionCounterArray[(counter*2)+1])*realdeltax)+(realdeltax/2.0)
                 #total += De*(1-math.exp(-a*xj))**2
-                total += (VModel[(d-1)-counter].De)*((1.0-math.exp(-(VModel[(d-1)-counter].a)*(xj-(LValue[(d-1)-counter]*0.5))))**2.0)
+                total += (VModel[(d-1)-counter][1])*((1.0-math.exp(-(VModel[(d-1)-counter][2])*(xj-(LValue[(d-1)-counter]*0.5))))**2.0)
             else:
                 pass
                 #This should never happen
     return(total)        
 
-#The function to calculate a VMatrix using the mol class from input
-def VMatrixCalc(mol, D):
+def VBlockCalc(dimensions, NValue, LValue, VType, VModel, blockX, blockY):
+    #Blocks will be 0 index
+    blockHolder = np.zeros((NValue[0], NValue[0]), float)
+    #The 0Start variables will always be 0 at the beginning to act as loop variables that correspond to the blockHolder size
+    alpha0start = 0
+    beta0start = 0
+    for alpha in range(0+NValue[0]*blockX, NValue[0]+NValue[0]*blockX):
+        for beta in range(0+NValue[0]*blockY, NValue[0]+NValue[0]*blockY):
+            counter = pyfghutil.AlphaAndBetaToCounter(alpha, beta, dimensions, NValue)
+            blockHolder[alpha0start, beta0start] = (Vab(dimensions, NValue, LValue, VType, VModel, 0, counter))
+            beta0start += 1
+        alpha0start += 1
+        beta0start = 0
+    return blockHolder
+
+#The function to calculate a VMatrix using the DataObject class input
+def VMatrixCalc(dataObj):
     #Establish variables needed
-    dimensions = D
-    NValue = mol.N
-    LValue = mol.L
-    VModel = mol.Vmodel
+    NValue = []
+    LValue = []
+    #Create the NValue and LValue list from scratch:
+    if(int(dataObj.holdData.N1) > 0):
+        NValue.append(int(dataObj.holdData.N1))
+        LValue.append(int(dataObj.holdData.L1))
+    if(int(dataObj.holdData.N2) > 0):
+        NValue.append(int(dataObj.holdData.N2))
+        LValue.append(int(dataObj.holdData.L2))
+    if(int(dataObj.holdData.N3) > 0):
+        NValue.append(int(dataObj.holdData.N3))
+        LValue.append(int(dataObj.holdData.L3))
+    
+    dimensions = len(NValue)
+    VType = []
+    VModel = []
+    for VModelClass in dataObj.holdData.model_data:
+        VType.append(VModelClass.type)
+        VModel.append(VModelClass.param)
+
+    '''
+    VType = []
+    for VTypeString in dataObj.holdData.v:
+        if (VTypeString == "Harmonic Oscillator"):
+            VType.append(0)
+        elif (VTypeString == "Morse Oscillator"):
+            VType.append(1)
+        else:
+            VType.append(2)
+    
     VType = mol.Vtype
-    #Reference a mol.Vmodel[0].k
-    mu = mol.mu
-    #deltax = (float(LValue)/float(NValue))
+    '''
     #Create the array for the x dimensional counters
+
+    #This will be configured to work with 2D only at the moment
+    
     dimensionCounterArray = np.zeros((dimensions*2,1), int)
 
     #Create the VMatrix
     #The alpha and beta values are used to create the VMatrix in the correct position
     vmatrix = np.zeros((np.prod(NValue), np.prod(NValue)), float)
 
-    #Calculate the VMatrix
-    for i in range((np.prod(NValue))*(np.prod(NValue))):
-        alpha = pyfghutil.AlphaCalc(dimensions, dimensionCounterArray, NValue)
-        beta = pyfghutil.BetaCalc(dimensions, dimensionCounterArray, NValue)  
+    #NBlock Class System
+    NBlocks = NBlock(dimensions, NValue)
+    NBlocks.difSetup()
+    print(NBlocks.D)
+    print(NBlocks.N)
+    print(NBlocks.diagblocks)
+    print(NBlocks.difblocks)
+    print(NBlocks.NProd)
+    '''
+    NBlocks.print1x1()
+    
+    print("Try to access")
+    print(NBlocks.accessPoint(37, 20))
+    '''
+    
+    #Calculate by blocks:
+    #Don't optimize for now. Just calculate blocks as needed.
+    blockCoords = []
+    optBlockCoords = []
+    blocks = []
+    paramz = []
+    totalwidth = int(np.prod(NValue))
+    repeatamount = int(totalwidth // NValue[0])
+    #print(NValue)
+    for x in range(repeatamount):
+        for y in range(repeatamount):
+            blockCoords.append((x,y))
+    optBlockCoords = NBlocks.coordGen()
+    #print(optBlockCoords)
+    for coords in optBlockCoords:
+        paramz.append((dimensions, NValue, LValue, VType, VModel, coords[0], coords[1]))
+        
+    p = mp.Pool(16)
+    print("Pool go V")
+    blocks = p.starmap(VBlockCalc, paramz)
+    print("Pool's done V")
+    p.close()
 
-        vmatrix[alpha, beta] = (Vab(dimensions, NValue, LValue, VModel, VType, 0, dimensionCounterArray))
-
-        #Adds +1 to the last dimension's X/Y value and checks to see if values need to add 1 to the next dimension counter / sets the current value to 0
-        dimensionCounterArray = pyfghutil.DCAAdvance(dimensions, dimensionCounterArray, NValue)
+    precalc = 0
+    for i in range(len(optBlockCoords)):
+        block = blocks[i]
+        x = optBlockCoords[i][0]
+        y = optBlockCoords[i][1]
+        #vmatrix[(0+NValue[precalc]*x):(NValue[precalc]+NValue[precalc]*x), (0+NValue[precalc]*y):(NValue[precalc]+NValue[precalc]*y)] = block
+        #print("Block: "+str(x)+", "+str(y))
+        NBlocks.setBlock(x, y, block)
+    for i in range(len(blockCoords)):
+        x = blockCoords[i][0]
+        y = blockCoords[i][1]
+        vmatrix[(0+NValue[precalc]*x):(NValue[precalc]+NValue[precalc]*x), (0+NValue[precalc]*y):(NValue[precalc]+NValue[precalc]*y)] = NBlocks.readBlock(x,y)
+        
+    
     return vmatrix
 
+    
+    #Calculate the VMatrix for alpha and beta loop test
+    '''
+    for alpha in range(np.prod(NValue)):
+        for beta in range(np.prod(NValue)):
+            dimensionCounterArray = pyfghutil.AlphaAndBetaToCounter(alpha, beta, D, NValue)
+            vmatrix[alpha, beta] = (Vab(dimensions, NValue, LValue, VModel, VType, 0, dimensionCounterArray))
+    return vmatrix
+    '''
+    
 
 
 
+'''
 D = 1
 N = [5]
 L = [3]
@@ -93,3 +309,4 @@ Vmodel = [HarmonicOscillatorModel(0.37)]
 
 mol = Molecule(D, N, L, mu, Vtype, Vmodel)
 print(VMatrixCalc(mol,D))
+'''
--- main.py
+++ main.py
@@ -1,26 +1,150 @@
-import inputparam
-import Tmatrix
-import Vmatrix
-import scipy
-import numpy as np
-import sys
+import os
+import time
+import paramiko
+import GUI
+from util import DataObject
+from multiprocessing import Process, Queue
 
-#Remove the numpy print limits
-np.set_printoptions(threshold=sys.maxsize)
+import GUItoCalc as GTC
 
-D = inputparam.inputValidInt("\nEnter the number of dimensions:\n",1,5)
-Vtype = inputparam.inputValidInt("\nEnter Potential Energy Method (0 = model, 1 = file):\n",0,1)
-mol = inputparam.Molecule(D,Vtype)
+"""
+This one uses Queues
+This communicates by using SSH
+Also connected to test3.py
+Author: Josiah Randleman
+© Copyright 2021, Josiah Randleman, All rights reserved. jrandl516@gmail.com
+"""
 
-for i in range(0,D):
-    #Currently broken
-    print ("the number of points in dimension " + str(i+1) + " is " + str(mol.N[i]))
 
-#hmatrix = matfunc.mdhmatrix(mol, D, Vtype)
-hmatrix = Tmatrix.TMatrixCalc(mol, D, Vtype) + Vmatrix.VMatrixCalc(mol, D, Vtype)
-print(hmatrix)
-print("hmatrix done")
-hmateigenv = scipy.linalg.eigvals(hmatrix)
-heigensort = np.sort(hmateigenv)
-print("eigendone")
-print(heigensort)
+class Bob:
+    def __init__(self):
+        self.sum = 0
+
+    def setSum(self, sum):
+        self.sum = sum
+        return
+
+
+# This is the parent process
+def datamuncher(q):
+    print('This is the parent process: ', os.getpid())
+    holder1 = q.get()
+    print(holder1.message)
+    print("Molecule: ", holder1.molecule)
+    print("Q1 equation: ", holder1.q_equation1)
+    print("Q2 equation: ", holder1.q_equation2)
+    print("Q3 equation: ", holder1.q_equation3)
+    print("N1: ", holder1.N1)
+    print("L1: ", holder1.L1)
+    print("N2: ", holder1.N2)
+    print("L2: ", holder1.L2)
+    print("N3: ", holder1.N3)
+    print("L3: ", holder1.L3)
+    print("T : ", holder1.t)
+    print("G : ", holder1.g)
+    print("Filename: ", holder1.file_name)
+    print("V : ", holder1.v)
+    print("Values from the sum of N and L: ", holder1.sum)
+    data = [holder1.molecule, holder1.q_equation1, holder1.q_equation2,
+            holder1.q_equation3, holder1.N1, holder1.L1, holder1.N2,
+            holder1.L2, holder1.N3, holder1.L3, holder1.t,
+            holder1.g, holder1.v]
+    save_path = "./resources/"
+    file_name = "DataList.txt"
+    completeName = os.path.join(save_path, file_name)
+    file = open(completeName, "w", encoding="utf-8")
+    for x in data:
+        file.write('%s\n' % x)
+    file.close()
+
+    DataObject.holdData.user2 = holder1.user
+
+
+    def SSH_connection():
+        host = holder1.host
+        port = 22
+        username = holder1.user
+        password = holder1.password
+        command = "python3 test3.py"
+        command2 = "rm DataList.txt"
+        command3 = "rm test3.py"
+        command4 = "rm Results.txt"
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, port, username, password)
+        sftp = ssh.open_sftp()
+
+        path = "/home/" + username + "/DataList.txt"
+        localpath = "./resources/DataList.txt"
+        sftp.put(localpath, path)
+        path2 = "/home/" + username + "/test3.py"
+        localpath2 = "test3.py"
+        sftp.put(localpath2, path2)
+        stdin, stdout, stderr = ssh.exec_command(command)
+        stdin, stdout, stderr = ssh.exec_command(command2)
+        stdin, stdout, stderr = ssh.exec_command(command3)
+        path3 = "/home/" + username + "/Results.txt"
+        localpath3 = "./resources/Results.txt"
+        sftp.get(path3, localpath3)
+        stdin, stdout, stderr = ssh.exec_command(command4)
+        sftp.close()
+        ssh.close()
+
+
+
+    if holder1.remote == 'Yes':
+        SSH_connection()
+
+    Charles = Bob()
+    Charles.setSum(holder1.sum)
+    q.put(Charles)
+    # print("File Name : ", holder1.file_name, " This is from the child process")
+    # print("Model Data : ", holder1.model_data, " This is from the child process")
+    return
+
+
+def datagrabber():
+    q = Queue()
+    p1 = Process(target=datamuncher, args=(q,))
+    p1.start()
+    time.sleep(1)
+    GUI.main_window()
+    print('The interface is started Process: ', os.getpid())
+    holder = DataObject.InputData()
+    holder.setMolecule(DataObject.holdData.molecule)
+    holder.setQ1(DataObject.holdData.q_equation1)
+    holder.setQ2(DataObject.holdData.q_equation2)
+    holder.setQ3(DataObject.holdData.q_equation3)
+    holder.setN1(DataObject.holdData.N1)
+    holder.setL1(DataObject.holdData.L1)
+    holder.setN2(DataObject.holdData.N2)
+    holder.setL2(DataObject.holdData.L2)
+    holder.setN3(DataObject.holdData.N3)
+    holder.setL3(DataObject.holdData.L3)
+    holder.setT(DataObject.holdData.t)
+    holder.setG(DataObject.holdData.g)
+    holder.setV(DataObject.holdData.v)
+    holder.setFileName(DataObject.holdData.file_name)
+    v = int(DataObject.holdData.N1) + int(DataObject.holdData.N2) + int(DataObject.holdData.N3) + int(
+        DataObject.holdData.L1) + \
+        int(DataObject.holdData.L2) + int(DataObject.holdData.L3)
+    holder.setMessage("This is from the child")
+    holder.set_sum(v)
+    holder.set_host(DataObject.holdData.host)
+    holder.set_user(DataObject.holdData.user)
+    holder.set_password(DataObject.holdData.password)
+    holder.set_remote(DataObject.holdData.remote)
+    # holder.setModelData(DataObject.holdData.model_data)  # look into pickling possibly un-pickling
+    q.put(holder)
+    Charles = q.get()
+    print(Charles.sum)
+
+    #At this point, insert the data into the handler
+    GTC.passToCalc(DataObject)
+    return
+    
+
+if __name__ == '__main__':
+    datagrabber()
+    print('done')

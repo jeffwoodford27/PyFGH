1640a682d479b2a385c52afad8eb468fac4c7697
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2022-02-15 12:50:45 -0600

    updated my molecules testing.py. This will check if an element is valid and will return an error message otherwise.

--- molecules testing.py
+++ molecules testing.py
@@ -1,5 +1,15 @@
 import csv
-with open('isotopes.csv', newline='') as csvfile:
-    pamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in pamreader:
-        print(', '.join(row))
+from typing import List
+from util import pyfghutil
+
+list2: list[str] = []
+with open("molecules.csv") as f:
+    for row in f:
+        list2.append(row[0])
+    for x in list2:
+        if x in pyfghutil.AtomicSymbolLookup.values():
+            print(f"'{x}' is a valid element")
+        else:
+            print(f"'{x}' is not a valid element")
+
+print(list2)

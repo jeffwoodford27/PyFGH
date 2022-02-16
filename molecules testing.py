dd13dbef2e89a5e6c381eb075195eeb997ac1e30
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2022-02-15 21:27:43 -0600

    updated my molecules testing.py file. to test it run my molecules testing.py file. It will read the molecules.csv file. It will read this file and will perform some validation checks. It will also look up the dictionaries in the pyfghutil.py file. It will print out each molecules info such as mass, number and symbol. Still working on it.

--- molecules testing.py
+++ molecules testing.py
@@ -1,15 +1,35 @@
-import csv
-from typing import List
 from util import pyfghutil
 
+holder = pyfghutil.Atom()
+Z = []
 list2: list[str] = []
+list3: list[str] = []
 with open("molecules.csv") as f:
     for row in f:
-        list2.append(row[0])
-    for x in list2:
+        list2.append(row.split('-')[0])
+        list3.append(row.split(',')[0])
+    for x in list2:  # x is the atomic symbol
         if x in pyfghutil.AtomicSymbolLookup.values():
-            print(f"'{x}' is a valid element")
+            print(x + " is a valid element")
         else:
-            print(f"'{x}' is not a valid element")
+            print(x + " is not a valid element")
 
-print(list2)
+    print("\n")
+    for x in list2:  # x is the atomic symbol
+        for key, value in pyfghutil.AtomicSymbolLookup.items():
+            if x == value:
+                holder.Z.append(key)
+                print("Atomic number of " + x + ":", key)
+
+    print("\n")
+    for x in list3:  # x is the atomic symbol
+        for key, value in pyfghutil.MassLookup.items():
+            if x == key:
+                holder.A.append(value)
+                print("Atomic mass of " + x + ":" , value)
+
+
+print("\n")
+print("Atomic Number: ", holder.Z)  # Atomic Number
+print("Atomic Mass: ", holder.A)  # Atomic Mass
+print("Symbol: ", list2)  # Symbol

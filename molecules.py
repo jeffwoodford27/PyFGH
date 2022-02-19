709d4c0817f180e26a3faf3574fc02395b0c8de0
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2022-02-19 15:35:05 -0600

    updated my molecules.py to work with the atom class properly. It saves it to the class now.

--- molecules.py
+++ molecules.py
@@ -1,15 +1,19 @@
+import self
+
 from util import pyfghutil
 import math
 
-holder = pyfghutil.Atom()
+# holder = pyfghutil.Atom()
 Z = []
 s = []
-list2: list[str] = []
-list3: list[str] = []
-x_coordinates: list[str] = []
-y_coordinates: list[str] = []
-z_coordinates: list[str] = []
-mass: list[str] = []
+A = []
+m = []
+list2 = []
+list3 = []
+x_coordinates = []
+y_coordinates = []
+z_coordinates = []
+mass = []
 
 # TODO For atom molecule Atom(Z, A, m, x y). Implement this into this here.
 # TODO molecules Li 6,  Li 7, Ce 20. Fix this in the program.
@@ -18,22 +22,9 @@ mass: list[str] = []
 # TODO do distance test with potential energy file which is x1 and so forth
 # TODO waterpot.csv: Q1, Q2, Q3, x1, y1, x2, y2, x3, y3, energies
 
-from collections.abc import Iterable
-
-
-def flatten(lis):
-    for item in lis:
-        if isinstance(item, Iterable) and not isinstance(item, str):
-            for x in flatten(item):
-                yield x
-        else:
-            yield item
-
-
 with open("molecules.csv") as f:
     for row in f:
         list2.append(row.split(',')[0])  # Li
-        # list3.append((row.split(' ')[0]) + (row[1])) # Li-6
         new_row = ['-'.join([row.split(',')[0], row.split(',')[1]])]
         x = ' '.join(new_row)
         list3.append(x)
@@ -42,17 +33,16 @@ with open("molecules.csv") as f:
         z_coordinates.append((row.split(',')[4]).strip())
         mass.append(row.split(',')[1])
         masslist = ([float(x) for x in mass])
-        #print(list2)  # ['Li']
+        # print(list2)  # ['Li']
         print(list3)  # ['Li-6']
         print("\n")
     for x in list2:
         print(x)
 
-
     for x in list2:  # x is the atomic symbol
         if x in pyfghutil.AtomicSymbolLookup.values():
             print(x + " is a valid element")
-            holder.s.append(x)
+            s.append(x)
         else:
             print(x + " is not a valid element")
 
@@ -60,7 +50,7 @@ with open("molecules.csv") as f:
     for x in list2:  # x is the atomic symbol
         for key, value in pyfghutil.AtomicSymbolLookup.items():
             if x == value:
-                holder.Z.append(key)
+                Z.append(key)
                 print("Atomic number of " + x + ":", key)
 
     print("\n")
@@ -70,7 +60,7 @@ with open("molecules.csv") as f:
                 print("valid atomic mass")
 
             if x == key:
-                holder.A.append(value)
+                A.append(value)
                 print("Atomic mass of " + x + ":", value)
 
     print("\n")
@@ -80,6 +70,10 @@ with open("molecules.csv") as f:
                 print(x + " is a valid element")
                 if value in masslist:
                     print("valid atomic mass")
+    for x in A:
+        atomic = x * 1822.89
+        m.append(atomic)
+        print("amu to atomic ", m)
 
 
 # Function to find distance
@@ -137,7 +131,9 @@ print("X Coordinates of the elements: ", xlist)
 print("Y Coordinates of the elements: ", ylist)
 print("Z Coordinates of the elements: ", zlist)
 print("\n")
-print("Atomic Number: ", holder.Z)  # Atomic Number
-print("Atomic Mass: ", holder.A)  # Atomic Mass
-print("Symbol: ", holder.s)  # Symbol
+print("Atomic Number: ", Z)  # Atomic Number
+print("Atomic Mass: ", A)  # Atomic Mass
+print("Symbol: ", s)  # Symbol
 calculations(x1, y1, z1, x2, y2, z2, xx2, yy2, zz2, xx3, yy3, zz3)
+test = pyfghutil.Atom(Z, A, m, xlist, ylist)
+print(getattr(test, 'A'))

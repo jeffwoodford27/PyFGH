078b937fb82b9324370cb8e1b490bb5cf312c321
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2022-03-06 20:36:35 -0600

    updated the molecules.py file to validate Q1,Q2,Q3. I am still working on this. Need to go back and check my values to see if I am correct. I added new code on lines 264-301.

--- molecules.py
+++ molecules.py
@@ -1,9 +1,14 @@
+import csv
+
 import self
 
-from util import pyfghutil
+from util import pyfghutil, DataObject
 import math
 
 # holder = pyfghutil.Atom()
+Q1 = []
+Q2 = []
+Q3 = []
 Z = []
 s = []
 A = []
@@ -21,13 +26,49 @@ poty1 = []
 poty2 = []
 poty3 = []
 
-# TODO For atom molecule Atom(Z, A, m, x y). Implement this into this here.
-# TODO molecules Li 6,  Li 7, Ce 20. Fix this in the program.
 # TODO ask the user for two files. File 1 is the equilibrium file or the molecules.csv file
 # TODO File 2 is the potential energies file which is the waterpot-data.csv
-# TODO do distance test with potential energy file which is x1 and so forth
-# TODO waterpot.csv: Q1, Q2, Q3, x1, y1, x2, y2, x3, y3, energies
+# TODO read in q and energies and verify in the water.csv that # of lines equals n1*n2*n3.
+# TODO q must be equally spaced also q3 then q2 then q1. Calcalte the Detla Q. L1=n1*delta Q1;  L2=n2*delta Q2
+# do distance test with potential energy file which is x1 and so forth
+# waterpot.csv: Q1, Q2, Q3, x1, y1, x2, y2, x3, y3, energies
+# TODO add to interface!!!!!!
 
+totalN = 0
+#totalN = DataObject.holdData.N1 * DataObject.holdData.N2 * DataObject.holdData.N3
+
+
+N1 = 10
+N2 = 10
+N3 = 10
+
+
+def getNs():
+    file = open("waterpot-data.csv")
+    reader = csv.reader(file)
+    lines = len(list(reader))
+    totalN = N1*N2*N3
+    if lines == totalN:
+        print("The lines of waterpot-data equals the toal of all N vales")
+    else:
+        print("The lines of waterpot-data do not equal the toal of all N vales ", lines)
+    return totalN
+"""
+def validateQ ():
+    #TODO calculate delta q
+    with open("waterpot-data.csv", ) as f:
+        for x in f:
+            Q1.append(x.split(',')[0])
+            Q2.append(x.split(',')[1])
+            Q3.append(x.split(',')[2])
+
+        i = 0
+        while i < len(Q3):
+            if Q3[i] == Q3[0] and (Q2[i] - 1.00):
+                print('true')
+                break
+
+"""
 with open("molecules.csv") as f:
     for row in f:
         list2.append(row.split(',')[0])  # Li
@@ -196,7 +237,7 @@ This does validation checking for the waterpot-data.csv file
 for x in range(
         len(potxlist1) and len(potxlist2) and len(potxlist3) and len(potylist1) and len(potylist2) and len(potylist3)):
     calculations2(potxlist1[x], potylist1[x], potxlist2[x], potylist2[x], potxlist3[x], potylist3[x])
-    x += 1
+
 
 print("\n")
 print("WaterPot for x1: ", potxlist1)
@@ -214,5 +255,46 @@ print("Atomic Number: ", Z)  # Atomic Number
 print("Atomic Mass: ", A)  # Atomic Mass
 print("Symbol: ", s)  # Symbol
 calculations(x1, y1, z1, x2, y2, z2, xx2, yy2, zz2, xx3, yy3, zz3)
-test = pyfghutil.Atom(Z, A, m, xlist, ylist)
+test = pyfghutil.Atom(Z, A, m, xlist, ylist)  # save only one molecule to the atom class. Save a collection of the atoms to the structure class.
 print(getattr(test, 'Z'))
+getNs()
+#validateQ()
+
+"""
+This is for validating the water potentila energy file
+"""
+
+N1_2 = 7
+L1_2 = 0.7
+N2_2 = 7
+L2_2 = 0.7
+N3_2 = 7
+L3_2 = 0.7
+
+deltaQ1 = L1_2 / float(N1_2)
+deltaQ2 = L2_2 / float(N2_2)
+deltaQ3 = L3_2 / float(N3_2)
+
+kenobi = 1
+with open("waterpot-data.csv", ) as f:
+    for x in f:
+        Q1.append(x.split(',')[0])
+        Q2.append(x.split(',')[1])
+        Q3.append(x.split(',')[2])
+
+
+for i in range(N1_2):
+    for j in range(N2_2):
+        for k in range(N3_2):
+            q1 = deltaQ1*float(i - float(N1_2 / 2))
+            q2 = deltaQ2*float(j - float(N2_2 / 2))
+            q3 = deltaQ3*float(k - float(N3_2 / 2))
+            print("Q1: ", q1)
+            print("Q2: ", q2)
+            print("Q3: ", q3)
+            print(Q1[k], Q2[k], Q3[k])
+            if (q1 == Q1[kenobi-1]) and (q2 == Q2[kenobi-1]) and (q3 == Q3[kenobi-1]):
+                print("it passes!")
+            else:
+                print("throw an error!")
+            kenobi += 1

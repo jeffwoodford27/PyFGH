797bbf8d325b0aac94d14db97ca862c42ed1f52d
unknown <nelsonmaxey@gmail.com>
2022-03-17 02:49:38 -0500

    Cleanup and wrap up. Use main.py to run.

--- Tmatrix.py
+++ Tmatrix.py
@@ -2,29 +2,22 @@
 import numpy as np
 import scipy as scipy
 import scipy.linalg
-from util import pyfghutil
+from util import pyfghutil, DataObject
 import math
 import multiprocessing as mp
 import sys
-
+import pandas
 
 #A function to calculate the BMatrix
 def bmatrixgen(NValue, LValue):
-    #Generate the BMatrix
+    n = int((NValue - 1)/2)
     b_matrix = np.zeros((NValue, NValue), float)
-    #Generate the difference matrix for the value calculations
-    difb_matrix = np.zeros((NValue, 1), float)
-    for a in difb_matrix:
-        for b in range(int((NValue-1)/2)):
-            difb_matrix[int(a)] += ((b+1)*math.sin(( (b+1)*2.0*math.pi*int(a))/ NValue))
-        difb_matrix[int(a)] *= (4.0*math.pi)/(LValue*NValue)
-    #Use the dif_bmatrix to set the values of the BMatrix
-    for y in range(NValue):
-        for x in range(NValue):
-            if (x-y) > 0:
-                b_matrix[x, y] = (-1*difb_matrix[abs(x-y)])
-            if (x-y) <= 0:
-                b_matrix[x, y] = difb_matrix[abs(x-y)]
+    for j in range(NValue):
+        for l in range(NValue):
+            for p in range(1, n+1):
+                b_matrix[j][l] += float(p)*math.sin(2*math.pi*float(p)*float(l-j)/float(NValue))
+            b_matrix[j][l] *= (4.0*math.pi)/(LValue*float(NValue))
+    
     return(b_matrix)
 
 #A function to calculate the CMatrix
@@ -45,10 +38,16 @@ def cmatrixgen(NValue, LValue):
     return c_matrix_local
 
 #A function to calculate the individual values for the TMatrix
-def Tab(d, NValue, LValue, mu, c_matrix_insert, dimensionCounterArray, approximation, b_matrix_insert = None, GMat = None):
+def Tab(d, NValue, LValue, mu, c_matrix_insert, dimensionCounterArray, approximation, b_matrix_insert, GMat):
     #Deltacounter is used to makes sure that the value being calculated is in the diagonal of the matrix
     Deltacounter = 0
     #Total is return value
+    def delta(x,y):
+        if(x==y):
+            return float(1)
+        else:
+            return float(0)
+    
     total = 0.0
     if(approximation == 4):
         for T in range(d):
@@ -64,7 +63,7 @@ def Tab(d, NValue, LValue, mu, c_matrix_insert, dimensionCounterArray, approxima
             if (Deltacounter == (d-1)):
                     #print(dimensionCounterArray)
                     try:
-                        total += (c_matrix_insert[T][dimensionCounterArray[(T*2)+1], dimensionCounterArray[T*2]])*((-1.0*1.0**2)/(2.0*mu[(d-1)-T])) 
+                        total += (-1.0/2.0)*(GMat[T][T])*(c_matrix_insert[T][dimensionCounterArray[(T * 2) + 1], dimensionCounterArray[T * 2]])
                     except:
                         print("Trying to access: "+str(dimensionCounterArray[(T*2)+1])+", "+str(dimensionCounterArray[T*2]))
                         print(c_matrix_insert[T])
@@ -78,8 +77,7 @@ def Tab(d, NValue, LValue, mu, c_matrix_insert, dimensionCounterArray, approxima
                     if(dimensionCounterArray[Ccounter*2] == dimensionCounterArray[(Ccounter*2)+1]):
                         Deltacounter += 1
             if(Deltacounter == d-1):
-                total += float(GMat.getPoint(C,C,dimensionCounterArray)) * (c_matrix_insert[C][dimensionCounterArray[(C*2)+1], dimensionCounterArray[C*2]])
-                #print("Accessed GMat point "+str(C)+", "+str(C)+" with dimension counter array: "+str(dimensionCounterArray))
+                total += float((GMat[C][C])) * (c_matrix_insert[C][dimensionCounterArray[(C*2)+1], dimensionCounterArray[C*2]])              
         #Perform all of the "B" calculations second
         for B in range(d):
             if(dimensionCounterArray[B*2] == dimensionCounterArray[(B*2)+1]):
@@ -92,35 +90,39 @@ def Tab(d, NValue, LValue, mu, c_matrix_insert, dimensionCounterArray, approxima
                 GRange.remove(B)
                 Gx = min(GRange)
                 Gy = max(GRange)
-                total +=  float(GMat.getPoint(Gx,Gy,dimensionCounterArray)) * temptotal * 2
-                #print("Accessed GMat point "+str(Gx)+", "+str(Gy)+" with dimension counter array: "+str(dimensionCounterArray))
+                total +=  float((GMat[Gx][Gy])) * temptotal * 2
         #Outside of summation multiplication of -hbar^2/2
         total *= (-1.0*1.0**2)/(2.0)
     elif(approximation == 2):
-        #Perform all of the "C" calcuations first
-        for C in range(d):
-            Deltacounter = 0
-            for Ccounter in range(d):
-                if(Ccounter != C):
-                    if(dimensionCounterArray[Ccounter*2] == dimensionCounterArray[(Ccounter*2)+1]):
-                        Deltacounter += 1
-            if(Deltacounter == d-1):
-                #print("This is what GMat lookup found at point "+str(dimensionCounterArray[1])+", "+str(dimensionCounterArray[3])+", "+str(dimensionCounterArray[5])+": "+str(GMat.getPoint(dimensionCounterArray[1],dimensionCounterArray[3],dimensionCounterArray[5])))
-                point = float(GMat.getPoint(C,C,dimensionCounterArray))
-                total += point * (c_matrix_insert[C][dimensionCounterArray[(C*2)+1], dimensionCounterArray[C*2]]) 
-        #Perform all of the "B" calculations second
-        for B in range(d):
-            if(dimensionCounterArray[B*2] == dimensionCounterArray[(B*2)+1]):
-                temptotal = 1.0
-                for BSecond in range(d):
-                    if(BSecond != B):
-                        #DeltaConstant currently fills the deltaG / deltaq constant
-                        deltaConst = 1  
-                        temptotal *= b_matrix_insert[BSecond][dimensionCounterArray[(BSecond*2)+1], dimensionCounterArray[BSecond*2]] * deltaConst
-                #This will be the first set of numbers and then the flipped ones     
-                total +=  0 * temptotal * 2
-        total *= (-1.0*1.0**2)/(2.0)
+        t = int(dimensionCounterArray[0])
+        j = int(dimensionCounterArray[1])
+        u = int(dimensionCounterArray[2])
+        k = int(dimensionCounterArray[3])
+        v = int(dimensionCounterArray[4])
+        l = int(dimensionCounterArray[5])
+        #BMatrix calls are backwards
+        #So BMatrix[0] would be B1, but it's actually B3
+        sums = 0.0
+        for p in range(NValue[0]):
+            sums += b_matrix_insert[2][j,p]*b_matrix_insert[2][p,t]*GMat[p][k][l][0][0]
+        total += -0.5*sums * delta(k,u) * delta(l,v)
 
+        sums = 0.0
+        for p in range(NValue[1]):
+            sums += b_matrix_insert[1][k,p]*b_matrix_insert[1][p,u]*GMat[j][p][l][1][1]
+        total += -0.5*sums*delta(j,t) * delta(l,v)
+
+        sums = 0.0
+        for p in range(NValue[2]):
+            sums += b_matrix_insert[0][l,p]*b_matrix_insert[0][p,v]*GMat[j][k][p][2][2]
+        total += -0.5*sums*delta(j,t)*delta(k,u)
+        
+        total += -0.5*(b_matrix_insert[2][j,t]*b_matrix_insert[1][k,u]*GMat[t][k][l][0][1]) * delta(v,l)
+        total += -0.5*(b_matrix_insert[2][j,t]*b_matrix_insert[0][l,v]*GMat[t][k][l][0][2]) * delta(k,u)
+        total += -0.5*(b_matrix_insert[1][k,u]*b_matrix_insert[2][j,t]*GMat[j][u][l][1][0]) * delta(v,l)
+        total += -0.5*(b_matrix_insert[1][k,u]*b_matrix_insert[0][l,v]*GMat[j][u][l][1][2]) * delta(j,t)
+        total += -0.5*(b_matrix_insert[0][l,v]*b_matrix_insert[2][j,t]*GMat[j][k][v][2][0]) * delta(k,u)
+        total += -0.5*(b_matrix_insert[0][l,v]*b_matrix_insert[1][k,u]*GMat[j][k][v][2][1]) * delta(j,t)
 
 
 
@@ -131,7 +133,7 @@ def Tab(d, NValue, LValue, mu, c_matrix_insert, dimensionCounterArray, approxima
     return(total)        
     #return(total*((-1.0*1.0**2)/(2.0*mu)))        
 
-def TBlockCalc(dimensions, NValue, LValue, mu, c_matrix, approximation, blockX, blockY, b_matrix=None, gmatrix=None):
+def TBlockCalc(dimensions, NValue, LValue, mu, c_matrix, approximation, blockX, blockY, b_matrix, gmatrix):
     #Blocks will be 0 index
     blockHolder = scipy.zeros((NValue[0], NValue[0]), float)
     #The 0Start variables will always be 0 at the beginning to act as loop variables that correspond to the blockHolder size
@@ -140,18 +142,37 @@ def TBlockCalc(dimensions, NValue, LValue, mu, c_matrix, approximation, blockX, 
     for alpha in range(0+NValue[0]*blockX, NValue[0]+NValue[0]*blockX):
         for beta in range(0+NValue[0]*blockY, NValue[0]+NValue[0]*blockY):
             counter = pyfghutil.AlphaAndBetaToCounter(alpha, beta, dimensions, NValue)
-            blockHolder[alpha0start, beta0start] = (Tab(dimensions, NValue, LValue, mu, c_matrix, counter, approximation, b_matrix, gmatrix))
+            if(approximation > 2):
+                counter1 = int(counter[0])
+                counter2 = int(counter[2])
+                counter3 = int(counter[4])
+                blockHolder[alpha0start, beta0start] = Tab(dimensions, NValue, LValue, mu, c_matrix, counter, approximation, b_matrix, gmatrix[counter1][counter2][counter3])
+            else:
+                blockHolder[alpha0start, beta0start] = Tab(dimensions, NValue, LValue, mu, c_matrix, counter, approximation, b_matrix, gmatrix)
             beta0start += 1
         alpha0start += 1
         beta0start = 0
     return blockHolder
 
 #The function to calculate a TMatrix using the mol class from input
-def TMatrixCalc(NValue, LValue, mu, TApprox, dimensions, g_matrix):
-    #Move deltax to functions that need it
-    #deltax = (float(LValue)/float(NValue))
-    #Create the array for the x dimensional counters
-    dimensionCounterArray = scipy.zeros((dimensions*2,1), int)
+def TMatrixCalc(dataObject, GMatrix):
+    #Establish variables needed
+    NValue = []
+    LValue = []
+    if(int(dataObject.N1) > 0):
+        NValue.append(int(dataObject.N1))
+        LValue.append(float(dataObject.L1))
+    if(int(dataObject.N2) > 0):
+        NValue.append(int(dataObject.N2))
+        LValue.append(float(dataObject.L2))
+    if(int(dataObject.N3) > 0):
+        NValue.append(int(dataObject.N3))
+        LValue.append(float(dataObject.L3))
+    D = len(NValue)
+    pes = dataObject.PES
+    dimensionCounterArray = scipy.zeros(D*2, int)
+    mu = []
+    Tapprox = 2
 
     #Create the TMatrix and the TFlagMatrix
     #The alpha and beta values are used to create the TMatrix in the correct position
@@ -159,43 +180,39 @@ def TMatrixCalc(NValue, LValue, mu, TApprox, dimensions, g_matrix):
     tflag = scipy.zeros((np.prod(NValue), np.prod(NValue)), int)
     alpha = 0
     beta = 0
-
-    #This will be configured to only work with 2D for now
     
     #Create the C_Matrix
     c_matrix = []
     for x in reversed(range(len(NValue))):
-        #c_matrix.append(cmatrixgen(NValue[(dimensions-1)-x], LValue[(dimensions-1)-x]))
         c_matrix.append(cmatrixgen(NValue[x], LValue[x]))
 
     #Create the B_Matrix if necessary
-    if(TApprox < 4):
+    if(Tapprox < 4):
         b_matrix = []
         for x in reversed(range(len(NValue))):
             b_matrix.append(bmatrixgen(NValue[x], LValue[x]))
+    else:
+        b_matrix = None
 
     blockCoords = []
     blocks = []
     paramz = []
     totalwidth = int(np.prod(NValue))
     repeatamount = int(totalwidth // NValue[0])
-    #print(repeatamount)
-    #print(NValue)
-    #Don't optimize for now. Just calculate blocks as needed.
     for x in range(repeatamount):
         for y in range(repeatamount):
             blockCoords.append((x,y))
-    if(TApprox == 4):
+    if(Tapprox == 4):
         for coords in blockCoords:
-            paramz.append((dimensions, NValue, LValue, mu, c_matrix, TApprox, coords[0], coords[1]))
-    elif(TApprox < 4):
+            paramz.append((D, NValue, LValue, mu, c_matrix, Tapprox, coords[0], coords[1], None, GMatrix))
+    elif(Tapprox < 4):
         for coords in blockCoords:
-            paramz.append((dimensions, NValue, LValue, mu, c_matrix, TApprox, coords[0], coords[1], b_matrix, g_matrix))
+            paramz.append((D, NValue, LValue, mu, c_matrix, Tapprox, coords[0], coords[1], b_matrix, GMatrix))
     else:
         #Occurs when invalid or unsupported T Approximation is used
-        pass 
-
-    p = mp.Pool(16)
+        pass
+    #Pool and run
+    p = mp.Pool(dataObject.cores_amount)
     print("Pool go T")
     blocks = p.starmap(TBlockCalc, paramz)
     print("Pool's done T")
@@ -210,17 +227,7 @@ def TMatrixCalc(NValue, LValue, mu, TApprox, dimensions, g_matrix):
     
     
     return tmatrix
-    '''
-    #Calculate the TMatrix with just alpha beta loop for testing
-    for alpha in range(np.prod(NValue)):
-        for beta in range(np.prod(NValue)):
-            #dimensionCounterArray = pyfghutil.AlphaAndBetaToCounter(alpha, beta, D, NValue)
-            #print("Alpha: "+str(alpha)+" Beta: "+str(beta)+" and Counter: ")
-            #print(dimensionCounterArray)
-            if tflag[alpha, beta] == 0:
-                dimensionCounterArray = pyfghutil.AlphaAndBetaToCounter(alpha, beta, D, NValue)
-                tmatrix[alpha, beta] = (Tab(dimensions, NValue, LValue, mu, c_matrix, dimensionCounterArray))
-                tflag[alpha,beta] = 1
-    return tmatrix
-    '''
+    
+    
+
 
--- Vmatrix.py
+++ Vmatrix.py
@@ -34,13 +34,6 @@ class NBlock:
         self.difblocks = []
         self.xboffset = 0
         self.yboffset = 0
-        #print(NValues)
-        #print("D and NProd"+str(self.D)+" "+str(self.NProd))
-        #print("Smallest N:"+str(self.SmallestN))
-        #We assume that blocks is an (Nx+1)^2 grid for blocks below it
-        #Shape is (y, x) of the array
-        #NDarray uses [y,x] accessing
-        #NProd will be the amount of items in alpha/beta it encompases for a block
         if(self.D > 1):
             self.diagblocks = [None]*self.N
             self.difblocks = [None]*(self.N-1)
@@ -58,15 +51,11 @@ class NBlock:
                 self.diagblocks[x].xboffset = self.xboffset + int(x * (self.NProd/self.SmallestN))
                 self.diagblocks[x].yboffset = self.yboffset + int(x * (self.NProd/self.SmallestN))
                 self.diagblocks[x].difSetup()
-                #print("Setting diag block: "+str(self.D-1)+" with xboffset "+str(self.xboffset + int(x * (self.NProd/self.SmallestN))))
-                #print("Setting diag block: "+str(self.D-1)+" with yboffset "+str(self.yboffset + int(x * (self.NProd/self.SmallestN))))
                         
             for y in range(self.N-1):
                 self.difblocks[y].xboffset = self.xboffset + 0
                 self.difblocks[y].yboffset = self.yboffset + 1+int(y * (self.NProd/self.SmallestN))
                 self.difblocks[y].difSetup()
-                #print("Setting diag block: "+str(self.D-1)+" with xboffset "+str(self.xboffset))
-                #print("Setting diag block: "+str(self.D-1)+" with yboffset "+str(self.yboffset + 1+int(y * (self.NProd/self.SmallestN))))
                 
     def coordGen(self):
         if(self.D>1):
@@ -81,11 +70,8 @@ class NBlock:
             
     def print1x1(self):
         if(self.D != 1):
-            '''
-            print("Print dimension class "+str(self.D-1)+" with N "+str(self.blocks[0,0].N)+"and with nprod "+str(self.blocks[0,0].NProd))
-            print(self.blocks[0,0].blocks)
-            self.blocks[0,0].print1x1()
-            '''
+            blank = 0
+            #Nothing
         else:
             pass
     def accessPoint(self, alpha, beta):
@@ -110,7 +96,6 @@ class NBlock:
             if(self.D > 1):
                 xb = N1BlockX//self.NProd
                 yb = N1BlockY//self.NProd
-                #print("Set block (D,xb,yb): "+str(self.D)+", "+str(xb)+", "+str(yb))
                 if(xb == yb):
                     self.diagblocks[xb].setBlock(N1BlockX%self.NProd, N1BlockY%self.NProd, blockData, False)
                 else:
@@ -146,7 +131,7 @@ class NBlock:
 
 
 #A function to calculate the invidivdual values for the VMatrix
-def Vab(d, NValue, LValue, VType, VModel, deltax, dimensionCounterArray):
+def Vab(d, NValue, LValue, deltax, pes, dimensionCounterArray):
     #Deltacounter is used to makes sure that the value being calculated is in the diagonal of the matrix
     Deltacounter = 0
     #Total is the value returned for the calculation
@@ -158,22 +143,11 @@ def Vab(d, NValue, LValue, VType, VModel, deltax, dimensionCounterArray):
     #If the deltacounter amount equals the amount of dimensions, perform a summation for the formula
     #Otherwise, the total will remain 0.0 
     if (Deltacounter == d):
-        for counter in range(d):
-            realdeltax = (float(LValue[(d-1)-counter])/float(NValue[(d-1)-counter]))
-            if(VType[counter] == 0):
-                xj = ((dimensionCounterArray[(counter*2)+1])*realdeltax)+(realdeltax/2.0)
-                total += 0.5*VModel[(d-1)-counter][1]*(xj-(LValue[(d-1)-counter]*0.5))**2
-            elif(VType[counter] == 1):
-                xj = ((dimensionCounterArray[(counter*2)+1])*realdeltax)+(realdeltax/2.0)
-                #total += De*(1-math.exp(-a*xj))**2
-                total += (VModel[(d-1)-counter][1])*((1.0-math.exp(-(VModel[(d-1)-counter][2])*(xj-(LValue[(d-1)-counter]*0.5))))**2.0)
-            else:
-                pass
-                #This should never happen
+        total += pes.getPointByN(int(dimensionCounterArray[1]),int(dimensionCounterArray[3]),int(dimensionCounterArray[5])).getEnergy()
         
     return(total)        
 
-def VBlockCalc(dimensions, NValue, LValue, VType, VModel, blockX, blockY):
+def VBlockCalc(dimensions, NValue, LValue, pes, blockX, blockY):
     #Blocks will be 0 index
     blockHolder = np.zeros((NValue[0], NValue[0]), float)
     #The 0Start variables will always be 0 at the beginning to act as loop variables that correspond to the blockHolder size
@@ -182,46 +156,30 @@ def VBlockCalc(dimensions, NValue, LValue, VType, VModel, blockX, blockY):
     for alpha in range(0+NValue[0]*blockX, NValue[0]+NValue[0]*blockX):
         for beta in range(0+NValue[0]*blockY, NValue[0]+NValue[0]*blockY):
             counter = pyfghutil.AlphaAndBetaToCounter(alpha, beta, dimensions, NValue)
-            blockHolder[alpha0start, beta0start] = (Vab(dimensions, NValue, LValue, VType, VModel, 0, counter))
+            blockHolder[alpha0start, beta0start] = (Vab(dimensions, NValue, LValue, 0, pes, counter))
             beta0start += 1
         alpha0start += 1
         beta0start = 0
     return blockHolder
 
 #The function to calculate a VMatrix using the DataObject class input
-def VMatrixCalc(dataObj):
+def VMatrixCalc(dataObject):
     #Establish variables needed
     NValue = []
     LValue = []
-    holder = DataObject.InputData()
-    #Create the NValue and LValue list from scratch:
-    if int(holder.N1) > 0:
-        NValue.append(int(holder.N1))
-        LValue.append(float(holder.L1))
-    if int(holder.N2) > 0:
-        NValue.append(int(holder.N2))
-        LValue.append(float(holder.L2))
-    if int(holder.N3) > 0:
-        NValue.append(int(holder.N3))
-        LValue.append(float(holder.L3))
-    
+    if(int(dataObject.N1) > 0):
+        NValue.append(int(dataObject.N1))
+        LValue.append(float(dataObject.L1))
+    if(int(dataObject.N2) > 0):
+        NValue.append(int(dataObject.N2))
+        LValue.append(float(dataObject.L2))
+    if(int(dataObject.N3) > 0):
+        NValue.append(int(dataObject.N3))
+        LValue.append(float(dataObject.L3))
+        
     dimensions = len(NValue)
-    '''
-    VType = []
-    for VTypeString in dataObj.InputData.v:
-        if (VTypeString == "Harmonic Oscillator"):
-            VType.append(0)
-        elif (VTypeString == "Morse Oscillator"):
-            VType.append(1)
-        else:
-            VType.append(2)
-    
-    VType = mol.Vtype
-    '''
-    #Create the array for the x dimensional counters
+    pes = dataObject.PES
 
-    #This will be configured to work with 2D only at the moment
-    
     dimensionCounterArray = np.zeros((dimensions*2,1), int)
 
     #Create the VMatrix
@@ -231,94 +189,40 @@ def VMatrixCalc(dataObj):
     #NBlock Class System
     NBlocks = NBlock(dimensions, NValue)
     NBlocks.difSetup()
-    print(NBlocks.D)
-    print(NBlocks.N)
-    print(NBlocks.diagblocks)
-    print(NBlocks.difblocks)
-    print(NBlocks.NProd)
-    '''
-    NBlocks.print1x1()
-    
-    print("Try to access")
-    print(NBlocks.accessPoint(37, 20))
-    '''
 
-    #If using model or file
-    if False:
-        #Calculate by blocks:
-        #Don't optimize for now. Just calculate blocks as needed.
-        blockCoords = []
-        optBlockCoords = []
-        blocks = []
-        paramz = []
-        totalwidth = int(np.prod(NValue))
-        repeatamount = int(totalwidth // NValue[0])
-        #print(NValue)
-        for x in range(repeatamount):
-            for y in range(repeatamount):
-                blockCoords.append((x,y))
-        optBlockCoords = NBlocks.coordGen()
-        #print(optBlockCoords)
-        for coords in optBlockCoords:
-            paramz.append((dimensions, NValue, LValue, VType, VModel, coords[0], coords[1]))
-        
-        p = mp.Pool(16)
-        print("Pool go V")
-        blocks = p.starmap(VBlockCalc, paramz)
-        print("Pool's done V")
-        p.close()
+
+    #Calculate by blocks:
+    #Don't optimize for now. Just calculate blocks as needed.
+    blockCoords = []
+    optBlockCoords = []
+    blocks = []
+    paramz = []
+    totalwidth = int(np.prod(NValue))
+    repeatamount = int(totalwidth // NValue[0])
+    for x in range(repeatamount):
+        for y in range(repeatamount):
+            blockCoords.append((x,y))
+    optBlockCoords = NBlocks.coordGen()
+    for coords in optBlockCoords:
+        paramz.append((dimensions, NValue, LValue, pes, coords[0], coords[1]))
+
+    #Pool and run
+    p = mp.Pool(dataObject.cores_amount)
+    print("Pool go V")
+    blocks = p.starmap(VBlockCalc, paramz)
+    print("Pool's done V")
+    p.close()
     
-        precalc = 0
-        for i in range(len(optBlockCoords)):
-            block = blocks[i]
-            x = optBlockCoords[i][0]
-            y = optBlockCoords[i][1]
-            NBlocks.setBlock(x, y, block)
-        for i in range(len(blockCoords)):
-            x = blockCoords[i][0]
-            y = blockCoords[i][1]
-            vmatrix[(0+NValue[precalc]*x):(NValue[precalc]+NValue[precalc]*x), (0+NValue[precalc]*y):(NValue[precalc]+NValue[precalc]*y)] = NBlocks.readBlock(x,y)
-    else:
-        #Using file
-        potentialfile = pandas.read_csv("waterpot-data.csv", names=['q1', 'q2', 'q3', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'v'])
-        countamount = int(np.prod(NValue))
-        potentialfilepreview = np.zeros((countamount, countamount), float)
-        for alpha in range(countamount):
-            for beta in range(countamount):
-                counterArray = pyfghutil.AlphaAndBetaToCounter(alpha, beta, dimensions, NValue)
-                deltas = 0
-                for coordPair in range(dimensions):
-                    if(counterArray[coordPair*2] == counterArray[coordPair*2 + 1]):
-                        deltas += 1
-                if(deltas == dimensions):
-                    vmatrix[alpha][beta] = potentialfile['v'][alpha]
-                    potentialfilepreview[alpha][beta] = potentialfile['v'][alpha]
-                #print("\n\n"+str(counterArray)+" alpha value "+str(alpha)+" beta value "+str(beta)+ " and value "+ str(potentialfile['v'][alpha]))
-        pandas.DataFrame(potentialfilepreview).to_csv("alphabetapreview.csv")
     
+    precalc = 0
+    for i in range(len(optBlockCoords)):
+        block = blocks[i]
+        x = optBlockCoords[i][0]
+        y = optBlockCoords[i][1]
+        NBlocks.setBlock(x, y, block)
+    for i in range(len(blockCoords)):
+        x = blockCoords[i][0]
+        y = blockCoords[i][1]
+        vmatrix[(0+NValue[precalc]*x):(NValue[precalc]+NValue[precalc]*x), (0+NValue[precalc]*y):(NValue[precalc]+NValue[precalc]*y)] = NBlocks.readBlock(x,y)
+
     return vmatrix
-
-    
-    #Calculate the VMatrix for alpha and beta loop test
-    '''
-    for alpha in range(np.prod(NValue)):
-        for beta in range(np.prod(NValue)):
-            dimensionCounterArray = pyfghutil.AlphaAndBetaToCounter(alpha, beta, D, NValue)
-            vmatrix[alpha, beta] = (Vab(dimensions, NValue, LValue, VModel, VType, 0, dimensionCounterArray))
-    return vmatrix
-    '''
-    
-
-
-
-'''
-D = 1
-N = [5]
-L = [3]
-mu = [919]
-Vtype = [0]
-Vmodel = [HarmonicOscillatorModel(0.37)]
-
-mol = Molecule(D, N, L, mu, Vtype, Vmodel)
-print(VMatrixCalc(mol,D))
-'''

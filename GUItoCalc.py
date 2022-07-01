edd044ecd0a874fa97096195317dc74f388e1449
jeffwoodford27 <jwoodford@missouriwestern.edu>
2022-07-01 16:49:01 -0500

    Updated GUItoCalc, implementing the new Eckart condition calculation.

--- GUItoCalc.py
+++ GUItoCalc.py
@@ -4,16 +4,48 @@ import Vmatrix
 import Tmatrix
 import Gmatrix
 import numpy as np
-import scipy
+from scipy import linalg
 import math
 from util.DataObject import OutputData
+from util import pyfghutil
 
-def eckartTranslation(dataObj):
-    equil = dataObj.getEquilMolecule()
-    pes = dataObj.getPES()
-    N1 = dataObj.getN1()
-    N2 = dataObj.getN2()
-    N3 = dataObj.getN3()
+def eckartTranslation(D,N,equil,pes):
+    Nat = equil.getNatom()
+    Npts = np.prod(N)
+    m = equil.getMassList()
+    M = 0.0
+    for i in range(3):
+        M += m[i]
+
+    for p in range(Npts):
+        idx = pyfghutil.PointToIndex(D,N,p)
+        mol = pes.getPointByIdx(idx)
+        xcm = ycm = zcm = 0.0
+        x = mol.getXList()
+        y = mol.getYList()
+        z = mol.getZList()
+        for j in range(Nat):
+            xcm += m[j]*x[j]
+            ycm += m[j]*y[j]
+            zcm += m[j]*z[j]
+        xcm = xcm/M
+        ycm = ycm/M
+        zcm = zcm/M
+        for j in range(Nat):
+            x[j] = x[j] - xcm
+            y[j] = y[j] - ycm
+            z[j] = z[j] - zcm
+        pes.getPointByIdx(idx).setXList(x)
+        pes.getPointByIdx(idx).setYList(y)
+        pes.getPointByIdx(idx).setZList(z)
+    return
+
+
+def eckartTranslationOld(D, N, equil, pes):
+    N1 = N[0]
+    N2 = N[1]
+    N3 = N[2]
+
     m = equil.getMassList()
     M = 0.0
     for i in range(3):
@@ -39,12 +71,86 @@ def eckartTranslation(dataObj):
                 pes.getPointByN(i,j,k).setYList(ynew)
     return
 
-def eckartRotation(dataObj):
-    equil = dataObj.getEquilMolecule()
-    pes = dataObj.getPES()
-    N1 = dataObj.getN1()
-    N2 = dataObj.getN2()
-    N3 = dataObj.getN3()
+def eckartRotation(D,N,equil,pes):
+    Nat = equil.getNatom()
+    Npts = np.prod(N)
+    m = equil.getMassList()
+
+    xe = equil.getXList()
+    ye = equil.getYList()
+    ze = equil.getZList()
+    for p in range(Npts):
+        idx = pyfghutil.PointToIndex(D,N,p)
+        mol = pes.getPointByIdx(idx)
+        x = mol.getXList()
+        y = mol.getYList()
+        z = mol.getZList()
+
+        A = np.zeros((3,3),dtype=float)
+        for i in range(Nat):
+            A[0,0] += m[i]*x[i]*xe[i]
+            A[0,1] += m[i]*x[i]*ye[i]
+            A[0,2] += m[i]*x[i]*ze[i]
+            A[1,0] += m[i]*y[i]*xe[i]
+            A[1,1] += m[i]*y[i]*ye[i]
+            A[1,2] += m[i]*y[i]*ze[i]
+            A[2,0] += m[i]*z[i]*xe[i]
+            A[2,1] += m[i]*z[i]*ye[i]
+            A[2,2] += m[i]*z[i]*ze[i]
+
+        A1 = np.matmul(A.T,A)
+        A2 = np.matmul(A,A.T)
+
+        eval1,evec1 = linalg.eig(A1)
+        eval2,evec2 = linalg.eig(A2)
+
+        eval1 = np.real(eval1)
+        eval2 = np.real(eval2)
+        evec1 = evec1.T
+        evec2 = evec2.T
+
+        sortidx1 = eval1.argsort()
+        sortidx2 = eval2.argsort()
+        evec1sort = np.zeros((3,3),dtype=float)
+        evec2sort = np.zeros((3,3),dtype=float)
+        for i in range(3):
+            for j in range(3):
+                evec1sort[i,j] = evec1[sortidx1[i],j]
+                evec2sort[i,j] = evec2[sortidx2[i],j]
+
+        cp1 = np.cross(evec1sort[0],evec1sort[1])
+        if (np.dot(cp1,evec1sort[2]) < 0):
+            evec1sort[2] *= -1.0
+
+        cp2 = np.cross(evec2sort[0],evec2sort[1])
+        if (np.dot(cp2,evec2sort[2]) < 0):
+            evec2sort[2] *= -1.0
+
+        T = np.zeros((3,3),dtype=float)
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    T[i,j] += evec1sort[k,i]*evec2sort[k,j]
+
+        xr = np.zeros(Nat, dtype=float)
+        yr = np.zeros(Nat, dtype=float)
+        zr = np.zeros(Nat, dtype=float)
+
+        for j in range(Nat):
+            xr[j] = T[0,0]*x[j] + T[0,1]*y[j] + T[0,2]*z[j]
+            yr[j] = T[1,0]*x[j] + T[1,1]*y[j] + T[1,2]*z[j]
+            zr[j] = T[2,0]*x[j] + T[2,1]*y[j] + T[2,2]*z[j]
+
+        pes.getPointByIdx(idx).setXList(xr)
+        pes.getPointByIdx(idx).setYList(yr)
+        pes.getPointByIdx(idx).setZList(zr)
+    return
+
+def eckartRotationOld(D, N, equil, pes):
+    N1 = N[0]
+    N2 = N[1]
+    N3 = N[2]
+
     m = equil.getMassList()
     xeq = equil.getXList()
     yeq = equil.getYList()
@@ -80,9 +186,12 @@ def passToCalc(dataObj):
     N[1] = dataObj.getN2()
     N[2] = dataObj.getN3()
 
+    equil = dataObj.getEquilMolecule()
+    pes = dataObj.getPES()
+
     print("Imposing Eckart conditions")
-    eckartTranslation(dataObj)
-    eckartRotation(dataObj)
+    eckartTranslation(D, N, equil, pes)
+    eckartRotation(D, N, equil, pes)
 
     print("Creating G Matrix")
     GMat = Gmatrix.calcGMatrix(D, N, dataObj.PES, dataObj.EquilMolecule)
@@ -96,7 +205,7 @@ def passToCalc(dataObj):
     HMat = VMat + TMat
 
     print("Calculating eigenvalues")
-    eigenval, eigenvec = scipy.linalg.eigh(HMat)
+    eigenval, eigenvec = linalg.eigh(HMat)
     eigenval = eigenval * 219474.6
 
     ResultObj = OutputData()

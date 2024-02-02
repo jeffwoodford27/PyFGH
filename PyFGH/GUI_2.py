02b8464cc2cd9042041344301089804095cfbd38
Weston Henning <whenning03@gmail.com>
2024-02-02 15:18:10 -0600

    Moved gui elements to separate file

--- PyFGH/GUI_2.py
+++ PyFGH/GUI_2.py
@@ -6,92 +6,11 @@ from tkinter.messagebox import showerror
 import multiprocessing
 import sys
 import numpy as np
-
+from PyFGH import GUI_Classes as guc
 from PyFGH import molecule_gui
+from PyFGH import Constants as co
 from PyFGH.util import DataObject as DataObject
 
-# class InputObj:
-    # def __init__(self):
-    #     self.D = 0
-    #     self.N = None
-    #     self.L = None
-    #     self.cores_amount = 0
-    #     self.NoEigen = 0
-    #     self.PEMethod = None
-    #     self.EigenMethod = None
-    #     #New
-    #     self.equilibrium_file = None
-    #     self.potential_energy_file = None
-    #     self.EquilMolecule = 0
-    #     self.PES = 0
-    #     self.Vmethod = 0
-    #     self.psi4method = None
-    #     self.gui = None
-
-
-
-class TextBoxFrame(ttk.LabelFrame):
-    def __init__(self, container, txt):
-        super().__init__(container)
-
-        self.config(text=txt)
-        self.config(labelanchor=tk.N)
-
-        self.txtboxstrvar = tk.StringVar()
-        self.txtbox = ttk.Entry(self, width=10, textvariable=self.txtboxstrvar)
-        self.txtbox.grid(column=0, row=0)
-
-    def get(self):
-        return self.txtbox.get()
-
-    def clear(self):
-        self.txtboxstrvar.set('')
-        return
-
-class LabelFrame(ttk.Frame):
-    def __init__(self, container, txt):
-        super().__init__(container)
-
-        self.lbl = ttk.Label(self, text=txt)
-        self.lbl.grid(column=0, row=0)
-
-class ButtonFrame(ttk.Frame):
-    def __init__(self, container, txt, f):
-        super().__init__(container)
-
-        self.txtboxbutton = ttk.Button(self, text=txt, command=f)
-        self.txtboxbutton.grid(column=0, row=0)
-
-class ComboboxFrame(ttk.LabelFrame):
-    def __init__(self, container, txt, dropdownlist):
-        super().__init__(container)
-
-        self.config(text=txt)
-        self.config(labelanchor=tk.N)
-
-        self.cboxstrvar = tk.StringVar()
-        self.cbox = ttk.Combobox(self, values=dropdownlist, width=30, textvariable=self.cboxstrvar)
-        self.cbox.state(['readonly'])
-        self.cbox.grid(column=0, row=0, sticky=tk.NSEW)
-
-    def get(self):
-        return self.cbox.get()
-
-    def clear(self):
-        self.cboxstrvar.set('')
-        return
-
-class ListboxFrame(ttk.LabelFrame):
-    def __init__(self, container, txt, dropdownlist):
-        super().__init__(container)
-
-        self.config(text=txt)
-        self.config(labelanchor=tk.N)
-
-        self.list_items = tk.Variable(value=dropdownlist)
-
-        self.listbox = tk.Listbox(self, height=1, listvariable=self.list_items)
-        self.listbox.grid(column=0, row=0, sticky=tk.N)
 
 class GUI(tk.Tk):
     def __init__(self, obj):
@@ -108,51 +27,51 @@ class GUI(tk.Tk):
         for i in range(5):
             self.rowconfigure(i,weight=3)
 
-        self.TitleFrame = LabelFrame(self, "A Python Implementation of the Fourier Grid Hamiltonian Method")
+        self.TitleFrame = guc.LabelFrame(self, "A Python Implementation of the Fourier Grid Hamiltonian Method")
         self.TitleFrame.config(borderwidth=5)
         self.TitleFrame.config(padding=(5,5,5,5))
         self.TitleFrame.config(relief='solid')
         self.TitleFrame.grid(columnspan=3, row=0)
 
         DimensionRange = [i for i in range(1,7)]
-        self.DimensionInput = ComboboxFrame(self, "Dimensions: ", DimensionRange)
+        self.DimensionInput = guc.ComboboxFrame(self, "Dimensions: ", DimensionRange)
         self.DimensionInput.grid(column=0, row=1)
 
         CoresRange = [i for i in range(1,multiprocessing.cpu_count()+1)]
-        self.CoresInput = ComboboxFrame(self, "Computer Cores: ", CoresRange)
+        self.CoresInput = guc.ComboboxFrame(self, "Computer Cores: ", CoresRange)
         self.CoresInput.grid(column=0, row=3)
 
         NumEigen = [i for i in range(1,11)]
-        self.NumEigenInput = ComboboxFrame(self, "Number of Eigenvalues: ", NumEigen)
+        self.NumEigenInput = guc.ComboboxFrame(self, "Number of Eigenvalues: ", NumEigen)
         self.NumEigenInput.grid(column=1, row=1)
 
-        PEMethod = ["Calculate With Psi4", "Read From File"]
-        self.PEMethodInput = ComboboxFrame(self, "PE Input Method: ", PEMethod)
+        PEMethod = [co.CPSI, co.READ]
+        self.PEMethodInput = guc.ComboboxFrame(self, "PE Input Method: ", PEMethod)
         self.PEMethodInput.grid(column=1, row=3)
 
-        EigenMethod = ["Full Matrix", "Sparse Matrix"]
-        self.EigenMethodInput = ComboboxFrame(self, "Eigenvalue Calculation Method: ", EigenMethod)
+        EigenMethod = [co.FMAT, co.SMAT]
+        self.EigenMethodInput = guc.ComboboxFrame(self, "Eigenvalue Calculation Method: ", EigenMethod)
         self.EigenMethodInput.grid(column=1, row=2)
 
-        self.GetValuesButton = ButtonFrame(self, "Get N, L Values", self.getNLvalues)
+        self.GetValuesButton = guc.ButtonFrame(self, "Get N, L Values", self.getNLvalues)
         self.GetValuesButton.grid(column=0, row=2)
 
-        self.AboutButton = ButtonFrame(self, "About", self.AboutButtonCommand)
+        self.AboutButton = guc.ButtonFrame(self, "About", self.AboutButtonCommand)
         self.AboutButton.grid(column=2, row=1)
 
-        self.HelpButton = ButtonFrame(self, "Help", self.HelpButtonCommand)
+        self.HelpButton = guc.ButtonFrame(self, "Help", self.HelpButtonCommand)
         self.HelpButton.grid(column=2, row=2)
 
-        self.ClearButton = ButtonFrame(self, "Clear", self.ClearButtonCommand)
+        self.ClearButton = guc.ButtonFrame(self, "Clear", self.ClearButtonCommand)
         self.ClearButton.grid(column=2, row=3)
 
-        self.ExitButton = ButtonFrame(self, "Exit", self.ExitButtonCommand)
+        self.ExitButton = guc.ButtonFrame(self, "Exit", self.ExitButtonCommand)
         self.ExitButton.grid(column=2, row=4)
 
-        self.GetEquilCoordButton = ButtonFrame(self, "Get Equilibrium Coordinates", self.GetEquilCoordCommand)
+        self.GetEquilCoordButton = guc.ButtonFrame(self, "Get Equilibrium Coordinates", self.GetEquilCoordCommand)
         self.GetEquilCoordButton.grid(column=0, row=4)
 
-        self.CalculateButton = ButtonFrame(self, "CALCULATE!", self.CalculateButtonCommand)
+        self.CalculateButton = guc.ButtonFrame(self, "CALCULATE!", self.CalculateButtonCommand)
         self.CalculateButton.grid(column=1, row=4)
 
     def getNLvalues(self):
@@ -175,11 +94,11 @@ class GUI(tk.Tk):
         NFrame = []
         LFrame = []
         for i in range(D):
-            Nobj = TextBoxFrame(NLwindow, "N"+str(i+1))
+            Nobj = guc.TextBoxFrame(NLwindow, "N"+str(i+1))
             Nobj.grid(column=0, row=i)
             NFrame.append(Nobj)
 
-            Lobj = TextBoxFrame(NLwindow, "L"+str(i+1))
+            Lobj = guc.TextBoxFrame(NLwindow, "L"+str(i+1))
             Lobj.grid(column=1, row=i)
             LFrame.append(Lobj)
 
@@ -233,7 +152,7 @@ class GUI(tk.Tk):
 
             return
 
-        getValuesButton = ButtonFrame(NLwindow, "Get Values", get_values)
+        getValuesButton = guc.ButtonFrame(NLwindow, "Get Values", get_values)
         getValuesButton.grid(columnspan=2, row=D)
         return
 
@@ -293,12 +212,12 @@ class GUI(tk.Tk):
         self.obj.NoEigen = self.NumEigenInput.get()
         self.obj.PEMethod = self.PEMethodInput.get()
         self.obj.gui = True
-        if self.EigenMethodInput.get() == "Sparse Matrix":
+        if self.EigenMethodInput.get() == co.SMAT:
             self.obj.setEigenvalueMethod(True)
         else:
             self.obj.setEigenvalueMethod(False)
         self.obj.psi4method = None
-        if self.obj.PEMethod == "Read From File":
+        if self.obj.PEMethod == co.READ:
             self.obj.setVmethod(self.obj.PEMethod)
             self.Read_Structures_Button()
             self.test(self.obj)
@@ -338,83 +257,9 @@ class GUI(tk.Tk):
         x = askopenfilename(title='File Explorer for Equilibrium Structure')
         self.obj.setequilibrium_file(x)
 
-    # def setequilibrium_file(self, equilibrium_file):
-    #     self.obj.equilibrium_file = equilibrium_file
-    #     return
-    #
-    # def setpotential_energy(self, potential_energy):
-    #     self.obj.potential_energy_file = potential_energy
-    #     return
-    #
-    # def getD(self):
-    #     return self.obj.D
-    #
-    # def getNlist(self):
-    #     return self.obj.N
-    #
-    # def getN(self,j):
-    #     return self.obj.N[j]
-    #
-    # def getLlist(self):
-    #     return self.obj.L
-    #
-    # def getL(self,j):
-    #     return self.obj.L[j]
-    #
-    # def getEquilFile(self):
-    #     return self.obj.equilibrium_file
-    #
-    # def getPESFile(self):
-    #     return self.obj.potential_energy_file
-    #
-    # def setEquilMolecule(self,eq):
-    #     self.obj.EquilMolecule = eq
-    #     return
-    #
-    # def setPES(self,pes):
-    #     self.obj.PES = pes
-    #     return
-    #
+
     def test(self, obj):
         eq, pes = molecule_gui.molecule_testing(obj)
         gc.collect()
         self.obj.setEquilMolecule(eq)
         self.obj.setPES(pes)
-
-    # def setVmethod(self,vmethod):
-    #     self.obj.Vmethod = vmethod
-    #     return
-    #
-    # def getVmethod(self):
-    #     return self.obj.Vmethod
-    #
-    # def getEquilMolecule(self):
-    #     return self.obj.EquilMolecule
-    #
-    # def getPES(self):
-    #     return self.obj.PES
-    #
-    # def getCoresAmount(self):
-    #     return int(self.obj.cores_amount)
-    #
-    # def getPsi4Method(self):
-    #     return self.obj.psi4method
-
- #   def getNumberOfEigenvalues(self):
-  #      return int(self.obj.NoEigen)
-
-    # def getEigenvalueMethod(self):
-    #     return self.obj.EigenMethod
-    #
-    # def setEigenvalueMethod(self, eigenmethod):
-    #     self.obj.EigenMethod = eigenmethod
-    #     return
-    #
-    # def getgui(self):
-    #     return self.obj.gui
-
-# obj = InputObj()
-# win = GUI(obj)
-# win.mainloop()
-# print(obj.D, obj.N, obj.L, obj.cores, obj.NoEigen, obj.PEMethod, obj.EigenMethod)
-
